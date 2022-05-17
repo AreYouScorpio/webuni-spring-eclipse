f37ecea13855d55f3e911433332fe124687ed71a
AreYouScorpio <akos.schweger@gmail.com>
2022-05-17 15:55:56 +0200

    s AirportTLController done

---
+++ airport/src/main/java/hu/webuni/airport/dto/AirportDto.java
@@ -0,0 +1,42 @@
+package hu.webuni.airport.dto;
+
+public class AirportDto {
+    private long id;
+    private String name;
+    private String iata;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIata() {
+        return iata;
+    }
+
+    public void setIata(String iata) {
+        this.iata = iata;
+    }
+
+
+    public AirportDto(long id, String name, String iata) {
+        this.id = id;
+        this.name = name;
+        this.iata = iata;
+    }
+
+    public AirportDto() {
+    }
+
+}
---
+++ airport/src/main/java/hu/webuni/airport/web/AirportTLController.java
@@ -0,0 +1,53 @@
+package hu.webuni.airport.web;
+
+import hu.webuni.airport.dto.AirportDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class AirportTLController {
+
+    int a;
+
+    private List<AirportDto> allAirports = new ArrayList<>();
+    {
+        allAirports.add( new AirportDto(1, "Ferenc Liszt Airport", "BUD"));
+
+        System.out.println(allAirports);
+    }
+
+
+
+
+
+    @GetMapping("/")
+    public String home() {
+        System.out.println("hello");
+        System.out.println(a);
+        return "index";
+    }
+
+    @GetMapping("/airports")
+    public String listAirports(Map<String, Object> model){
+        model.put("airports", allAirports);
+        model.put("newAirport", new AirportDto());
+        return "airports";
+
+    }
+
+    @PostMapping("/airports")
+    public String addAirport(AirportDto airport){
+        allAirports.add(airport);
+        return "redirect:airports";
+
+    }
+
+
+
+}
