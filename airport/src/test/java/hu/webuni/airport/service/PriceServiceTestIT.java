18de4a1c0c775f8bbe5d8c5313bce2d1c7aae3b1
AreYouScorpio <akos.schweger@gmail.com>
2022-05-29 19:08:35 +0200

    profile switcher added to integration tests

--- airport/src/test/java/hu/webuni/airport/service/PriceServiceTestIT.java
+++ airport/src/test/java/hu/webuni/airport/service/PriceServiceTestIT.java
@@ -9,10 +9,12 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
+@ActiveProfiles("test") // profile switcher: prod off or on (test on or off)
 public class PriceServiceTestIT {
 
     @Autowired
@@ -25,10 +27,18 @@ public class PriceServiceTestIT {
     }
 
 
+    // prod on
     @Test
     void testGetFinalPriceWithHighPrice() throws Exception {
         int newPrice = priceService.getFinalPrice(11000);
         assertThat(newPrice).isEqualTo(9350);
+    }
+
+    // prod off
+    @Test
+    void testGetFinalPriceWithHighPrice2() throws Exception {
+        int newPrice = priceService.getFinalPrice(11000);
+        assertThat(newPrice).isEqualTo(9900);
     }
 
 }
