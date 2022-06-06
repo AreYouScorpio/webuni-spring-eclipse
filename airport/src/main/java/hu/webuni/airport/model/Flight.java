c5b1a2377b5d211e78acfdf24632b94c99e541ae
AreYouScorpio <akos.schweger@gmail.com>
2022-06-06 12:35:11 +0200

    Flight Entity setting up

---
+++ airport/src/main/java/hu/webuni/airport/model/Flight.java
@@ -0,0 +1,60 @@
+package hu.webuni.airport.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+public class Flight {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private Airport takeoff;
+    private Airport landing;
+
+    private String flightNumber;
+    private LocalDateTime takeoffTime;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Airport getTakeoff() {
+        return takeoff;
+    }
+
+    public void setTakeoff(Airport takeoff) {
+        this.takeoff = takeoff;
+    }
+
+    public Airport getLanding() {
+        return landing;
+    }
+
+    public void setLanding(Airport landing) {
+        this.landing = landing;
+    }
+
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+
+    public void setFlightNumber(String flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+
+    public LocalDateTime getTakeoffTime() {
+        return takeoffTime;
+    }
+
+    public void setTakeoffTime(LocalDateTime takeoffTime) {
+        this.takeoffTime = takeoffTime;
+    }
+}
