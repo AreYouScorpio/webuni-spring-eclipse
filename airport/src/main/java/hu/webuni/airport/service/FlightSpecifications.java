012ad9a4dc5959069e881edea1073c1d0a6692e5
AreYouScorpio <akos.schweger@gmail.com>
2022-06-23 13:30:16 +0200

    metamodell build done

--- airport/src/main/java/hu/webuni/airport/service/FlightSpecifications.java
+++ airport/src/main/java/hu/webuni/airport/service/FlightSpecifications.java
@@ -1,13 +1,13 @@
 package hu.webuni.airport.service;
 
 import hu.webuni.airport.model.Flight;
+import hu.webuni.airport.model.Flight_;
 import org.springframework.data.jpa.domain.Specification;
 
 public class FlightSpecifications {
 
 public static Specification<Flight> hasId(long id) {
     return (root, cq, cb) -> cb.equal(root.get(Flight_.id), id);
-
 }
 
     {
