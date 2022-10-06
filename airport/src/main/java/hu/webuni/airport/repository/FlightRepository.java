7a8af0d0242e598dd78c0939e23af60b983722ee
AreYouScorpio <akos.schweger@gmail.com>
2022-10-06 15:21:33 +0200

    QueryDSL setup done

--- airport/src/main/java/hu/webuni/airport/repository/FlightRepository.java
+++ airport/src/main/java/hu/webuni/airport/repository/FlightRepository.java
@@ -4,10 +4,13 @@ import hu.webuni.airport.model.Airport;
 import hu.webuni.airport.model.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 public interface FlightRepository extends
         JpaRepository<Flight, Long>,
-        JpaSpecificationExecutor<Flight> {
+        JpaSpecificationExecutor<Flight>,
+        QuerydslPredicateExecutor<Flight> {
 
+    //QueryDSL 2. course-hoz ezt is hozzá kell adni, h Predicate is átadható legyen, az AirportService-ben (korábbi FlightSpecben )
 
 }
--- airport/src/main/java/hu/webuni/airport/service/AirportService.java
+++ airport/src/main/java/hu/webuni/airport/service/AirportService.java
@@ -1,7 +1,11 @@
 package hu.webuni.airport.service;
 
+import com.google.common.collect.Lists;
+import com.querydsl.core.types.ExpressionUtils;
+import com.querydsl.core.types.Predicate;
 import hu.webuni.airport.model.Airport;
 import hu.webuni.airport.model.Flight;
+import hu.webuni.airport.model.QFlight;
 import hu.webuni.airport.repository.AirportRepository;
 import hu.webuni.airport.repository.FlightRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +17,7 @@ import org.springframework.util.StringUtils;
 
 import javax.persistence.Access;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.*;
 
 @Service
@@ -145,6 +150,8 @@ public class AirportService {
         return flightRepository.save(flight);
     }
 
+    /* masodik tanfolyamban atirjuk QueryDSL-re:
+
     public List<Flight> findFlightsByExample(Flight example) {
         long id = example.getId();
         String flightNumber = example.getFlightNumber();
@@ -172,6 +179,55 @@ public class AirportService {
 
 
         return flightRepository.findAll(spec, Sort.by("id"));
+    }
+
+
+     */
+
+
+    public List<Flight> findFlightsByExample(Flight example) {
+        long id = example.getId();
+        String flightNumber = example.getFlightNumber();
+        String takeoffIata = null;
+        Airport takeoff = example.getTakeoff();
+        if (takeoff != null)
+            takeoffIata = takeoff.getIata();
+        LocalDateTime takeoffTime = example.getTakeoffTime();
+
+        //Specification<Flight> spec = Specification.where(null); // üres Specification, ami semmire nem szűr
+
+        // a FlightSpecifications feltételeit ide írjuk bele közvetlen --->
+
+        ArrayList<Predicate> predicates = new ArrayList<com.querydsl.core.types.Predicate>();
+
+        QFlight flight = QFlight.flight;
+
+        if (id > 0) {
+
+            // spec = spec.and(FlightSpecifications.hasId(id));
+            predicates.add(flight.id.eq(id));
+        }
+
+        if (StringUtils.hasText(flightNumber)) // SpringFramework-ös StringUtils
+            // spec = spec.and(FlightSpecifications.hasFlightNumber(flightNumber));
+            predicates.add(flight.flightNumber.startsWithIgnoreCase(flightNumber));
+
+        if (StringUtils.hasText(takeoffIata)) // SpringFramework-ös StringUtils
+            //spec = spec.and(FlightSpecifications.hasTakeoffIata(takeoffIata));
+            predicates.add(flight.takeoff.iata.startsWith(takeoffIata));
+
+        if (takeoffTime != null) // SpringFramework-ös StringUtils
+            //spec = spec.and(FlightSpecifications.hasTakeoffTime(takeoffTime));
+        {
+            LocalDateTime startOfDay =
+                    LocalDateTime.of(takeoffTime.toLocalDate(),
+                            LocalTime.MIDNIGHT);
+            predicates.add(flight.takeoffTime.between(startOfDay, startOfDay.plusDays(1)));
+
+        }
+
+        //return flightRepository.findAll(spec, Sort.by("id"));
+        return Lists.newArrayList(flightRepository.findAll(ExpressionUtils.allOf(predicates)));
     }
 
 }
